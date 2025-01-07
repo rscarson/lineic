82ddc545a06e0f59a6afc1fbdcea032aeb565839
Richard Carson <r.carson@calian.com>
2025-01-07 10:13:35 -0500

    Improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 //! ## Examples
 //!
 //! The simplest possible use of the library is mapping one range to another  
-//! Here we can map values in the range 0.0..=10.0 to the range 30.0..=35.0
+//! Here we can map values in the range `0.0..=10.0` to the range `30.0..=35.0`
 //! ```rust
 //! use lineic::interpolators::F32InterpolationBucket;
 //! let interpolator = F32InterpolationBucket::new(0.0..=10.0, [30.0], [35.0]);
@@ -28,7 +28,7 @@
 //! -----
 //!
 //! The target does not have to be a single value - here we interpolate across a pair of RGB values  
-//! The result is a smooth gradient from red to green for values in the range 0.0..=10.0
+//! The result is a smooth gradient from `red` to `green` for values in the range `0.0..=10.0`
 //! ```rust
 //! use lineic::interpolators::F32InterpolationBucket;
 //! let interpolator = F32InterpolationBucket::new(0.0..=10.0, [255.0, 0.0, 0.0], [0.0, 255.0, 0.0]);
@@ -38,7 +38,7 @@
 //! -----
 //!
 //! The library can also interpolate smoothly across multiple pairs of values  
-//! This example forms a sort of traffic light sequence, interpolating between red, yellow, and green
+//! This example forms a sort of traffic light sequence, interpolating between `red`, `yellow`, and `green`
 //!
 //! The range is reversed here to demonstrate that the library can handle that
 //!
@@ -56,11 +56,13 @@
 //! -----
 //!
 //! The types for the range and values do not need to the same  
-//! Here a f64 range is used to interpolate between u8 values
+//! Here a `f64` range is used to interpolate across `u8` values
 //! ```rust
 //! use lineic::LinearInterpolator;
+//!
 //! let interpolator: LinearInterpolator<'_, 3, f64, u8> =
 //!     LinearInterpolator::new(0.0..=10.0, &[[0, 255, 0], [255, 255, 0], [255, 0, 0]]);
+//!
 //! assert_eq!(interpolator.interpolate(5.0), [255, 255, 0]);
 //! assert_eq!(interpolator.interpolate(0.0), [0, 255, 0]);
 //! ```
